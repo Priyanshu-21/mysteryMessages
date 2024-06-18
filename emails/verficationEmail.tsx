880e5a79e63df537068be8964c581606ce868f70
Priyanshu Mishra <pmfanofms21@gmail.com>
2024-06-18 22:35:38 +0530

    1.0: SendEmailOTP

---
+++ emails/verficationEmail.tsx
@@ -0,0 +1,13 @@
+import {
+    Html, 
+    Head, 
+    Font,
+    Preview, 
+    Heading, 
+    Row, 
+    Section, 
+    Text, 
+    Button, 
+} from '@react-email/components';
+
+// Next task : - Import react-email and need to work on the remaining code for the template. 
