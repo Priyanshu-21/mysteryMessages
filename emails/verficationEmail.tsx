4849a84e149c3db17e45c62703ba60619f639d1b
Priyanshu Mishra <pmfanofms21@gmail.com>
2024-06-19 22:27:54 +0530

    1.0: EmailOtpVerify

--- emails/verficationEmail.tsx
+++ emails/verficationEmail.tsx
@@ -1,13 +1,49 @@
 import {
     Html, 
-    Head, 
+    Head,
     Font,
     Preview, 
     Heading, 
     Row, 
     Section, 
     Text, 
-    Button, 
+    Button,
+    Body, 
 } from '@react-email/components';
 
-// Next task : - Import react-email and need to work on the remaining code for the template. 
+interface verificationEmailProps {
+    username: string; 
+    otp: string; 
+}
+
+export default function verificationEmail({ username, otp }: verificationEmailProps) {
+    return (
+        <Html lang='en' dir='ltr'>
+            <Head>
+                <Heading as='h1'>Vefication Code</Heading>
+                <Font
+                fontFamily="Roboto"
+                fallbackFontFamily="Verdana"
+                webFont={{
+                url: "https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
+                format: "woff2",
+                    }}
+                fontWeight={400}
+                fontStyle="normal"
+                />
+            </Head>
+            <Preview>Here &apos;s your verfication code: {otp}</Preview>
+            <Section>
+                <Row>
+                    <Heading as='h2'>Hello {username},</Heading>
+                </Row>
+                <Row>
+                    <Text>
+                        Thanks for registering. Please use the following verfication code to complete your registration.
+                    </Text>
+                </Row>
+                <Text>{otp}</Text>
+            </Section>
+        </Html> 
+    );
+}
--- src/helpers/sendverfiedEmail.ts
+++ src/helpers/sendverfiedEmail.ts
@@ -1,2 +1,7 @@
 import { resend } from '@/lib/resend'; 
 
+import verificationEmail from '../../emails/verficationEmail';
+
+import { ApiResponse } from '@/types/apiResponse';
+
+// Need to right the method to handle the sendVerification email 
